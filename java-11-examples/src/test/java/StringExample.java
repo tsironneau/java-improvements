9643a686a9f9560168c6769468e837a463793afd
Tanguy Sironneau <tsironneau@ankama.com>
2019-05-10 18:39:38 +0200

    feat (java 11) : add some string examples

---
+++ java-11-examples/src/test/java/StringExample.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("Some tests to experience Java 11 String Methods")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class StringExample {
+
+    @BeforeAll
+    static void global_set_up() {
+    }
+
+    @BeforeEach
+    void set_up() {
+    }
+
+    @AfterEach
+    void tear_down() {
+    }
+
+    @Test
+    void should_be_empty_when_repeat_zero_times() {
+        var toRepeat = "toRepeat";
+        final String result = toRepeat.repeat(0);
+        Assertions.assertEquals("", result);
+    }
+
+    @Test
+    void empty_string_still_empty_after_repeat(){
+        var emptyString = "";
+        final String result = emptyString.repeat(Integer.MAX_VALUE);
+        Assertions.assertEquals("", result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "\t", "\n", "   "})
+    void should_be_blank(String blankString) {
+        Assertions.assertTrue(blankString.isBlank());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"@", "a", "$"})
+    void should_not_be_blank(String blankString) {
+        Assertions.assertFalse(blankString.isBlank());
+    }
+
+    private static void toStrip() {
+        final var toStrip = "   toStrip   ";
+        System.out.println(toStrip.strip());
+    }
+}
