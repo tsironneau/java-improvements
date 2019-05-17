7ebf83147b250e189ca00cbeea9e2c0ce2f8517d
Tanguy Sironneau <tsironneau@ankama.com>
2019-05-17 15:29:47 +0200

    feat (java 12) : add java 12 module

---
+++ java-12-examples/src/test/java/StringExample.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayName("Some tests to experience Java 12 String Methods")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class StringExample {
+}
