c770e4a7a3a70e2344ac7adfa0ac85d450098916
Tanguy Sironneau <tsironneau@ankama.com>
2019-05-23 13:13:59 +0200

    feat (java 12) : add examples for String API
    - indent
    - transform

--- java-12-examples/src/test/java/StringExample.java
+++ java-12-examples/src/test/java/StringExample.java
@@ -1,8 +1,32 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.*;
+import org.junit.platform.commons.util.StringUtils;
 
 @DisplayName("Some tests to experience Java 12 String Methods")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class StringExample {
+
+    @Test
+    void indent_add_leading_spaces_and_line_break(){
+        final String toIndent = "toIndent";
+        final String indented = toIndent.indent(3);
+        Assertions.assertEquals("   toIndent\n", indented);
+    }
+
+    @Test
+    void indent_negative_argument_removes_leading_spaces(){
+        final String toIndent = "    toIndent";
+        final String indented = toIndent.indent(-3);
+        Assertions.assertEquals(" toIndent\n", indented);
+    }
+
+    @Test
+    void transform_returns_a_modified_string_with_lambda(){
+        final String toTransform = "to transform";
+        final String transformed = toTransform
+                .transform(s -> s.replace("to", "is"))
+                .transform(s -> s.concat("ed"));
+
+        Assertions.assertEquals("is transformed", transformed);
+    }
+
 }
