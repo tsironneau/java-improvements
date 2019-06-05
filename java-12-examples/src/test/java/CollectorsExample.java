500148ace2abdc6f2641fabd090d608a211c9c22
Tanguy Sironneau <tsironneau@ankama.com>
2019-06-05 15:21:16 +0200

    feat (java 12) : add Collectors.teeing example

---
+++ java-12-examples/src/test/java/CollectorsExample.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@DisplayName("Some tests to experience Java 12 Collectors Methods")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class CollectorsExample {
+
+    @Test
+    public void teeing__sort_number_list_with_even_before_odd() {
+        //Create a Integer list from 0 to 9 with even number first and odd number after
+        final List<Integer> collect = IntStream.range(0, 10).boxed().collect(
+                Collectors.teeing(
+                        //Filter the even numbers
+                        Collectors.filtering(i -> i % 2 == 0, Collectors.toList()),
+                        //Filter the odd numbers
+                        Collectors.filtering(i -> i % 2 == 1, Collectors.toList()),
+                        //Merge previous lists
+                        (List<Integer> a, List<Integer> b) -> {
+                            a.addAll(b);
+                            return a;
+                        }
+                )
+        );
+
+        System.out.println(collect);
+    }
+
+}
