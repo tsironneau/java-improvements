065a92b5039c93c9addf478790608a38fb34002e
tsironneau <tanguy.sironneau@gmail.com>
2020-04-03 09:10:56 +0200

    feat (java14) : add a record with constructor to check parameters

---
+++ java-14-examples/src/test/java/PositivePointRecordTest.java
@@ -0,0 +1,24 @@
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
+import org.junit.jupiter.api.Assertions;
+
+public class PositivePointRecordTest {
+
+    @Property
+    void check_x_coordinates_cannot_be_negative(@ForAll() @IntRange(min = Integer.MIN_VALUE, max = -1) int x, @ForAll() int y) {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> new PositivePoint(x, y)
+        );
+    }
+
+    @Property
+    void check_y_coordinates_cannot_be_negative(@ForAll() int x, @ForAll() @IntRange(min = Integer.MIN_VALUE, max = -1) int y) {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> new PositivePoint(x, y)
+        );
+    }
+
+}
