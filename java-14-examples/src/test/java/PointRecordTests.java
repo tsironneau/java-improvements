97e5bbc96f769a5d5da543c950aae4294438987a
tsironneau <tanguy.sironneau@gmail.com>
2020-04-03 09:08:45 +0200

    feat (java14) : add a Point record and its unit property tests
    See also :
    https://blogs.oracle.com/javamagazine/know-for-sure-with-property-based-testing

---
+++ java-14-examples/src/test/java/PointRecordTests.java
@@ -0,0 +1,33 @@
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import org.junit.jupiter.api.Assertions;
+
+public class PointRecordTests {
+
+    @Property
+    void check_record_generated_getters(@ForAll() int x, @ForAll() int y){
+        final Point recordPoint = new Point(x, y);
+        System.out.println("Pretty print of the record : " + recordPoint);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(x, recordPoint.x()),
+                () -> Assertions.assertEquals(y, recordPoint.y())
+        );
+    }
+
+    @Property
+    void check_record_equals(@ForAll() int x, @ForAll() int y){
+        final Point recordPoint = new Point(x, y);
+        final Point recordPoint2 = new Point(x, y);
+        System.out.println("Pretty print of the record : " + recordPoint);
+        Assertions.assertEquals(recordPoint, recordPoint2);
+    }
+
+    @Property
+    void check_record_hashCode(@ForAll() int x, @ForAll() int y){
+        final Point recordPoint = new Point(x, y);
+        final Point recordPoint2 = new Point(x, y);
+        System.out.println("Pretty print of the record : " + recordPoint);
+        Assertions.assertEquals(recordPoint.hashCode(), recordPoint2.hashCode());
+    }
+
+}
